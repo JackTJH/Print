741db1162997ed91d7de98ce95d44aad461cc098
jacktjh <2392775474@qq.com>
2026-05-28 22:18:53 +0800

    chore: 保存路径改为桌面 PrintReceived，客户端 IP 默认 192.168.1.

--- print_client/client_gui.py
+++ print_client/client_gui.py
@@ -31,7 +31,7 @@ class ClientGUI(QMainWindow):
         conn_layout = QHBoxLayout(conn_group)
 
         conn_layout.addWidget(QLabel("IP:"))
-        self.ip_edit = QLineEdit()
+        self.ip_edit = QLineEdit("192.168.1.")
         self.ip_edit.setPlaceholderText("例如 192.168.1.100")
         self.ip_edit.setFixedWidth(160)
         conn_layout.addWidget(self.ip_edit)
